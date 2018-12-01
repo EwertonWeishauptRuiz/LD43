76cf2d555ae1e9b5323df6764e2edb991ba12da5
Ewerton <shakamaiden@gmail.com>
2018-12-01 11:01:53 +0100

    initial movement script

---
+++ Ludum Dare 43 - The Pirate's Curse/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+  Rigidbody rbd;
+
+  Transform parentPivot;
+
+  public float speed;
+  public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+    rbd = GetComponent<Rigidbody>();
+    parentPivot = GetComponent<Transform>().transform.parent.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+
+    parentPivot.transform.Rotate(0, x, 0);
+
+    rbd.AddForce(0, 0, speed);
+
+	}
+}
